3f381284c8726cd1ec8f70c8d1dd88c3265ccfb8
Isaac Yang <isaacyang@Isaacs-some-MacBook-Air.local>
2023-12-02 23:31:11 +0800

    victim commonality view

---
+++ dsimulator/victim_common_attribute.sql
@@ -0,0 +1,28 @@
+CREATE VIEW commonality AS
+WITH victim_info AS(
+    SELECT * FROM
+    inhabitant LEFT OUTER JOIN home USING(home_building_id), victim
+    WHERE inhabitant_id = victim_id
+)
+SELECT *
+FROM(
+    SELECT attribute_name, attribute_value, COUNT(*) AS attribute_cnt
+    FROM (
+        SELECT 'name' AS attribute_name, name AS attribute_value FROM victim_info
+        UNION ALL
+        SELECT 'home_building_id', CAST(home_building_id AS TEXT) FROM victim_info
+        UNION ALL
+        SELECT 'workplace_id', CAST(workplace_id AS TEXT) FROM victim_info
+        UNION ALL
+        SELECT 'gender', gender FROM victim_info
+        UNION ALL
+        SELECT 'scene_vertex_id', CAST(scene_vertex_id AS TEXT) FROM victim_info
+        UNION ALL 
+        SELECT 'min_of_death', CAST(min_of_death AS TEXT) FROM victim_info
+        UNION ALL
+        SELECT 'income_level', CAST(income_level AS TEXT) FROM victim_info
+    ) AS attributes
+    GROUP BY attribute_name, attribute_value
+    HAVING COUNT(*) > 2;
+)
+ORDER BY attribute_cnt DESC
